e74439eaba0b18d2e9bd5f79e3bbfc5c8a2bacfa
Henrik Bach <bach.henrik@gmail.com>
2023-12-15 22:18:26 +0100

    chore: Create a install-dev-tools.sh script

--- src/cpp/.devcontainer/base-scripts/install-dev-tools.sh
+++ src/cpp/.devcontainer/base-scripts/install-dev-tools.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 # install-dev-tools.sh
+
+export RUN="sudo -s"
 
 # [Optional] Uncomment this section to install additional OS packages.
 # HB: 1st/2nd: For Linux Kernel Development
-RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+$RUN << EOF
+    apt-get update && export DEBIAN_FRONTEND=noninteractive \
     && apt-get -y install build-essential cmake cppcheck valgrind clang lldb llvm gdb \
     && apt-get -y install --no-install-recommends \
             git ctags make cmake
+EOF
