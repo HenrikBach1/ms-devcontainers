747134dba08aa3d925b1175f7aec2afc7f92a515
Henrik Bach <bach.henrik@gmail.com>
2023-12-16 06:58:15 +0100

    Fix: Add X11 forwarding

--- src/cpp/.devcontainer/base-scripts/install-tools-debian.sh
+++ src/cpp/.devcontainer/base-scripts/install-tools-debian.sh
@@ -1,16 +1,18 @@
 #! /usr/bin/env bash
-# install-dev-tools.sh
+# install-tools-debian.sh
 
 export RUN="sudo -s"
 
 # [Optional] Uncomment this section to install additional OS packages.
 # HB: 1st/2nd: For Linux Kernel Development
 # HB: 3rd: For Yocto
+# HB: 4th: For X11 forwarding
 $RUN << EOF
     apt-get update && export DEBIAN_FRONTEND=noninteractive \
     && apt-get -y install build-essential cmake cppcheck valgrind clang lldb llvm gdb \
     && apt-get -y install --no-install-recommends \
             git ctags make cmake \
     && apt-get -y install --no-install-recommends \
-            wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop
+            wget git-core unzip make gcc g++ build-essential subversion sed autoconf automake texi2html texinfo coreutils diffstat python-pysqlite2 docbook-utils libsdl1.2-dev libxml-parser-perl libgl1-mesa-dev libglu1-mesa-dev xsltproc desktop-file-utils chrpath groff libtool xterm gawk fop \
+            xauth
 EOF
