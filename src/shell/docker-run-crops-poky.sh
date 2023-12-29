97cf2aa18353ba30f548f4ed406922616875259b
Henrik Bach <bach.henrik@gmail.com>
2023-12-29 10:39:02 +0100

    chore: Make the script independent of the user invoking the script

--- src/shell/docker-run-crops-poky.sh
+++ src/shell/docker-run-crops-poky.sh
@@ -2,4 +2,4 @@
 # docker-run-crops-poky.sh
 
 # docker run --rm -it -v myvolume:/workdir crops/poky --workdir=/workdir
-docker run -it -v "C:/Users/henri/OneDrive/Projects":"/projects" --name crops-yocto-poky crops/poky:latest
+docker run -it -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" --name crops-yocto-poky crops/poky:latest
