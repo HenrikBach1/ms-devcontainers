d94e7a362713ecaa4d66ecf7c00e3a5668214d0d
Henrik Bach <bach.henrik@gmail.com>
2023-12-29 16:54:30 +0100

    chore: Make the script fluent whether there is a container or not

--- src/shell/docker-run-crops-poky.sh
+++ src/shell/docker-run-crops-poky.sh
@@ -1,5 +1,18 @@
-#! /usr/bin/bash
-# docker-run-crops-poky.sh
+# -e DISPLAY=<host-ip>:0 -v /tmp/.X11-unix:/tmp/.X11-unix
 
-# docker run --rm -it -v myvolume:/workdir crops/poky --workdir=/workdir
-docker run -it -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" --name crops-yocto-poky crops/poky:latest
+container_name="crops-yocto-poky"
+image_name="crops/poky:latest"
+
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+    echo "Container exists..."
+    docker start ${container_name}
+    docker exec -it \
+        --user=pokyuser ${container_name} \
+        bash -c 'cd ~ && exec bash'
+else
+    echo "Container doesn't exists..."
+    docker run -it \
+        --name ${container_name} \
+        -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" \
+        ${image_name}
+fi
