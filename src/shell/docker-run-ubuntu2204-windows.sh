48e4445030b5709dd3468ad125fd6dd117813ac1
Henrik Bach <bach.henrik@gmail.com>
2023-12-29 15:54:57 +0100

    chore: Add X11-Window DISPLAY example

--- src/shell/docker-run-ubuntu2204-windows.sh
+++ src/shell/docker-run-ubuntu2204-windows.sh
@@ -1,1 +1,2 @@
+# -e DISPLAY=<host-ip>:0 -v /tmp/.X11-unix:/tmp/.X11-unix
 docker run -it --name=cpp-kernel-ubuntu2204 -v "C:/Users/henri":"/mnt/henri" -v "C:/Projects":"/projects" ubuntu:22.04 /bin/bash
