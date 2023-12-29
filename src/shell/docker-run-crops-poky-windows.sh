cc794c1ffc7a130aba1feee46db42359c6a7d05b
Henrik Bach <bach.henrik@gmail.com>
2023-12-29 20:56:31 +0100

    chore: Make docker-run-crops-windows.sh dependent on docker-run-helper.sh script

--- src/shell/docker-run-crops-poky-windows.sh
+++ src/shell/docker-run-crops-poky-windows.sh
@@ -1,18 +1,24 @@
 # -e DISPLAY=<host-ip>:0 -v /tmp/.X11-unix:/tmp/.X11-unix
 
+container_user=pokyuser
 container_name="crops-yocto-poky"
+container_volumes=" -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" "
+# container_cmd="bash -c 'cd ~ && exec bash'"
+container_cmd="/bin/bash"
 image_name="crops/poky:latest"
 
-if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
-    echo "Container exists..."
-    docker start ${container_name}
-    docker exec -it \
-        --user=pokyuser ${container_name} \
-        bash -c 'cd ~ && exec bash'
+# Get the directory path of the currently executing script
+current_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Construct the path to the script you want to source
+script_to_source="${current_script_dir}/docker-run-helper.sh"
+
+# Check if the script exists before sourcing it
+if [ -f "$script_to_source" ]; then
+    # Source the script
+    source "$script_to_source"
 else
-    echo "Container doesn't exists..."
-    docker run -it \
-        --name ${container_name} \
-        -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" \
-        ${image_name}
+    echo "Error: Script to source not found at $script_to_source"
 fi
+
+docker_run
