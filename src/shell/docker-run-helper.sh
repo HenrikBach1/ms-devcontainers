ea4501db74e239aed08414d96e3082fc8ff03c96
Henrik Bach <bach.henrik@gmail.com>
2023-12-30 09:19:46 +0100

    chore: Cleanup in code

--- src/shell/docker-run-helper.sh
+++ src/shell/docker-run-helper.sh
@@ -4,16 +4,33 @@ docker_run()
 {
     if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
         echo "Container exists..."
-        docker start ${container_name}
-        docker exec -it \
+        docker container start \
+            ${container_name}
+        # Only available in swarm
+        # docker volume update \
+        #     ${container_volumes} \
+        #     ${container_name}
+        docker container exec -it \
             --user=${container_user} \
             ${container_name} \
             ${container_cmd}
     else
-        echo "Container doesn't exists..."
-        docker run -it \
+        echo "Container doesn't exists. Creates container..."
+        docker container run -it \
             --name ${container_name} \
             ${container_volumes} \
             ${image_name}
     fi
 }
+
+# # Get the directory path of the currently executing script
+# current_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# script_to_source="${current_script_dir}/docker-run-helper.sh"
+
+# # Check if the script exists before sourcing it
+# if [ -f "$script_to_source" ]; then
+#     # Source the script
+#     source "$script_to_source"
+# else
+#     echo "Error: Script to source not found at $script_to_source"
+# fi
