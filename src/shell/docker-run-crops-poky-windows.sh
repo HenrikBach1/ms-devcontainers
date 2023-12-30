281f49ea2708b0920ae5cba40cc2bf5fee3d6366
Henrik Bach <bach.henrik@gmail.com>
2023-12-30 09:21:46 +0100

    chore: Cleanup in code

--- src/shell/docker-run-crops-poky-windows.sh
+++ src/shell/docker-run-crops-poky-windows.sh
@@ -1,24 +1,18 @@
-# -e DISPLAY=<host-ip>:0 -v /tmp/.X11-unix:/tmp/.X11-unix
+# -e DISPLAY=<host-ip>:0 --volume /tmp/.X11-unix:/tmp/.X11-unix
 
 container_user=pokyuser
 container_name="crops-yocto-poky"
-container_volumes=" -v "C:/Users/${USER}":"/mnt/${USER}" -v "C:/Projects":"/projects" "
-# container_cmd="bash -c 'cd ~ && exec bash'"
+    # --volume "C:/Users/${USER}":"/mnt/${USER}" 
+container_volumes=" \
+    --volume "C:/Projects":"/projects" \
+    --volume "C:/Users/${USER}/OneDrive/Projects/yocto/projects":"/projects/yocto/projects" \
+    --volume "C:/Users/${USER}/OneDrive/Projects/yocto/poky":"/projects/yocto/poky" \
+    --volume "C:/Users/${USER}/OneDrive/Projects/yocto/shared":"/projects/yocto/shared" \
+    "
 container_cmd="/bin/bash"
 image_name="crops/poky:latest"
 
-# Get the directory path of the currently executing script
 current_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Construct the path to the script you want to source
-script_to_source="${current_script_dir}/docker-run-helper.sh"
-
-# Check if the script exists before sourcing it
-if [ -f "$script_to_source" ]; then
-    # Source the script
-    source "$script_to_source"
-else
-    echo "Error: Script to source not found at $script_to_source"
-fi
+source "${current_script_dir}/docker-run-helper.sh"
 
 docker_run
