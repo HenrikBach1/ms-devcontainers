39d4bd52d70246fc31d59b7a66b497ace8591643
Henrik Bach <bach.henrik@gmail.com>
2024-01-02 13:11:49 +0100

    chore: Add script to install ROS2 Humble distribution

--- src/shell/install-tools-ros2-humble-ubuntu.sh
+++ src/shell/install-tools-ros2-humble-ubuntu.sh
@@ -3,10 +3,22 @@
 
 export RUN="sudo -s"
 
-apt update -y
+if [[ "${USER}" == "" ]]; then
+        if [[ ! ${HOME} == "" ]]; then
+            export USER=$(basename "$HOME")
+        else
+            echo "Cannot state which user is running in the terminal"
+            exit 1
+        fi
+fi
 
-if ! type "sudo" &> /dev/null; then
-    apt install -y sudo
+if [[ "${USER}" == "root" ]]; then
+    # Suppose first time installation
+    apt update -y
+
+    if ! type "sudo" &> /dev/null; then
+        apt install -y sudo
+    fi
 fi
 
 # [Optional] Uncomment this section to install additional OS packages.
@@ -17,17 +29,15 @@ $RUN << EOF
         build-essential cmake cppcheck valgrind clang lldb llvm gdb \
         nano less
     
-    # For Linux Kernel Development
-    apt-get -y install --no-install-recommends \
-        git make cmake exuberant-ctags 
-    
     # For ROS2 - Humble
     # Enable universe repos
     apt install software-properties-common
     add-apt-repository universe
     # Add the ROS 2 GPG key
     apt update -y && sudo apt install curl -y
-    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
+            -o /usr/share/keyrings/ros-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
     # Install ROS 2 packages
     apt update -y
     apt upgrade -y
