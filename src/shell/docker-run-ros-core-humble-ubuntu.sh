f80ac42ff30fb1f3d136d38a1731b779768fe7ca
Henrik Bach <bach.henrik@gmail.com>
2024-01-10 22:54:51 +0100

    chore: Rename script to include platform

--- src/shell/docker-run-ros-core-humble-ubuntu.sh
+++ src/shell/docker-run-ros-core-humble-ubuntu.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 # source C:\Users\henri\OneDrive\Projects\DevContainers\github\henrikbach1\ms-devcontainers\src\shell\
-    # docker-run-ros-core-humble.sh
+# source ~/projects/DevContainers/github/henrikbach1/ms-devcontainers/src/shell/
+    # docker-run-ros-core-humble-ubuntu.sh
 
 docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /mnt/wslg:/mnt/wslg \
-    --volume "C:/Projects":"/projects" \
+    --volume "${HOME}/Projects":"/projects" \
     -e DISPLAY=:0 \
     -e WAYLAND_DISPLAY=wayland-0 \
     -e XDG_RUNTIME=/mnt/wslg/runtime-dir \
     -e PULSE_SERVER=/mnt/wslg/PulseServer \
+    --name ros-core-humble
     ros:humble-ros-core
