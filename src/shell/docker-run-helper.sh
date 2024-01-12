5950b001d4983f03b28a6d355f43f960f1030821
Henrik Bach <bach.henrik@gmail.com>
2024-01-12 09:30:25 +0100

    chore: Adhere to script standard

--- src/shell/docker-run-helper.sh
+++ src/shell/docker-run-helper.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#! /usr/bin/env bash
+# docker-run-helper.sh
 
 docker_run()
 {
--- src/shell/docker-run-yocto-crops-poky-windows.sh
+++ src/shell/docker-run-yocto-crops-poky-windows.sh
@@ -1,4 +1,5 @@
-# -e DISPLAY=<host-ip>:0 --volume /tmp/.X11-unix:/tmp/.X11-unix
+#! /usr/bin/env bash
+# docker-run-yocto-crops-poky-windows.sh
 
 container_user=pokyuser
 container_name="crops-yocto-poky"
