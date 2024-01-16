3db243c00d345696944715cd5277cd7160433854
Henrik Bach <bach.henrik@gmail.com>
2024-01-16 11:03:11 +0100

    chore: Add error message and how to fix it as a TODO

--- src/shell/docker-run-ubuntu2204-linux.sh
+++ src/shell/docker-run-ubuntu2204-linux.sh
@@ -14,3 +14,30 @@ include_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${include_dir}/docker-run-helper.sh"
 
 docker_run
+
+# TODO: If below problem persists with: 
+# How do i make sure locale 'en_US.UTF-8' is available on your ubuntu system
+# How to fix invalid locale settings LANG=en_US.UTF8
+
+# Generating answers for you…
+
+# I’m sorry to hear that. Here’s how you can fix the error:
+
+# The error message indicates that the locale settings are invalid. You can try the following steps to fix it:
+
+# Open a terminal window.
+# Run the command sudo nano /etc/locale.gen to open the locale configuration file.
+# Uncomment the line en_US.UTF-8 UTF-8 by removing the # character at the beginning of the line.
+# Save the file and exit the editor.
+# Run the command sudo locale-gen to generate the new locale settings.
+# Run the command sudo update-locale LANG=en_US.UTF-8 to update the locale settings.
+# If you still encounter the same error, you can try the following:
+
+# Open the file /etc/default/locale with a text editor.
+# Add the following lines to the file:
+# LANG=en_US.UTF-8
+# LC_ALL=en_US.UTF-8
+
+# Save the file and exit the editor.
+# Run the command sudo update-locale LANG=en_US.UTF-8 to update the locale settings.
+# I hope this helps!
