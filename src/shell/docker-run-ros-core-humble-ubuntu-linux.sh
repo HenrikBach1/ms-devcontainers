254c6c0b78b3ab5bdd9c2ee90f837b83b14083c5
Henrik Bach <bach.henrik@gmail.com>
2024-01-22 22:18:38 +0100

    chore: Create script to run ros-core and x11 apps in docker on linux

--- src/shell/docker-run-ros-core-humble-ubuntu-linux.sh
+++ src/shell/docker-run-ros-core-humble-ubuntu-linux.sh
@@ -1,16 +1,22 @@
 #! /usr/bin/env bash
 # source C:\Users\henri\OneDrive\Projects\DevContainers\github\henrikbach1\ms-devcontainers\src\shell\
 # source ~/projects/DevContainers/github/henrikbach1/ms-devcontainers/src/shell/
-    # docker-run-ros-core-humble-ubuntu.sh
+    # docker-run-ros-core-humble-ubuntu-linux.sh
 
-docker run -it \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /mnt/wslg:/mnt/wslg \
-    -e DISPLAY=:0 \
-    -e WAYLAND_DISPLAY=wayland-0 \
-    -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir \
-    -e PULSE_SERVER=/mnt/wslg/PulseServer \
-    --name ros-core-humble \
+xhost +
+X11_CONTAINER_ARGS=" \
+    --volume $HOME/.Xauthority:/root/.Xauthority:rw \
+    -e DISPLAY=$DISPLAY \
+    --net=host \
+    "
+
+# include_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# source "${include_dir}/docker-run-helper.sh"
+# # docker_run
+
+    # --rm 
+docker \
+    run -it \
+    ${X11_CONTAINER_ARGS} \
     --volume "${HOME}/projects":"/projects" \
     ros:humble-ros-core
-    # --volume "/mnt/c/Users/${USER}/OneDrive/Projects":"/projects"
